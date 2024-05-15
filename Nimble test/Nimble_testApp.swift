6e084311022e3d9d3759ddaf72909338f8b97e1c
Grygorii Tarashchuk <grygoriitarashchuk@tests-MacBook-Pro.local>
2024-05-15 14:44:22 +0300

    Initial commit

---
+++ Nimble test/Nimble_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nimble_testApp.swift
+//  Nimble test
+//
+//  Created by Grygorii Tarashchuk on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Nimble_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
